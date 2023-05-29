0a299057ba8b701bee3085cdbcee5740ecb675db
kalyan.devkota <kalyan.devkota@ekbana.info>
2023-05-29 15:14:29 +0545

    chore: added global pipe and config swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
+  const config = new DocumentBuilder()
+    .setTitle('Finance tracker Demo API')
+    .setDescription(
+      'A REST API using Nestjs to create CRUD operations on finance tracker',
+    )
+    .setVersion('1.0')
+    .addTag('Finance-tracker')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api', app, document);
   await app.listen(3000);
 }
 bootstrap();
