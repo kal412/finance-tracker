1e33daee6ede98bad6ed381238fd67297a1589d2
kalyan.devkota <kalyan.devkota@ekbana.info>
2023-05-29 15:15:19 +0545

    chore: create constants

---
+++ src/common/constants/error-code-lists.constants.ts
@@ -0,0 +1,82 @@
+export const ErrorCodeConstant = {
+  '-1000': {
+    context: 'All interfaces',
+    errorCodeDescription: 'Unknown system abnormality, please contact',
+  },
+  '-1001': {
+    context: 'All interfaces',
+    errorCodeDescription: 'System internal abnormality, please contact',
+  },
+  '-1002': {
+    context: 'All interfaces',
+    errorCodeDescription: 'Parameter error, please check the passed parameter',
+  },
+  '-1005': {
+    context: 'All interfaces',
+    errorCodeDescription: 'Appid error',
+  },
+  '-1006': {
+    context: 'All interfaces',
+    errorCodeDescription: 'Appid does not exist, please contact',
+  },
+  '-1007': {
+    context: 'All interfaces',
+    errorCodeDescription: 'Signature error, please check signature.',
+  },
+  '-1008': {
+    context: 'All interfaces',
+    errorCodeDescription: 'Expired',
+  },
+  '-1009': {
+    context: 'All interfaces',
+    errorCodeDescription: 'Request too frequent',
+  },
+  '-1010': {
+    context: 'All interfaces',
+    errorCodeDescription: 'Illegal Ip',
+  },
+  '-1011': {
+    context: 'User Interface',
+    errorCodeDescription: 'Not Registered. Please register first',
+  },
+  '-1012': {
+    context: 'User Interface',
+    errorCodeDescription: 'User does not exist',
+  },
+  '-1013': {
+    context: 'User Interface',
+    errorCodeDescription: 'User already exists',
+  },
+  '-1014': {
+    context: 'User Interface',
+    errorCodeDescription: 'User banned',
+  },
+  '-1015': {
+    context: 'User Interface',
+    errorCodeDescription: 'User password error',
+  },
+  '-1020': {
+    context: 'Wallet Interface',
+    errorCodeDescription: 'Insufficient user balance',
+  },
+  '-1021': {
+    context: 'Wallet Interface',
+    errorCodeDescription: 'The deposit and withdrawl amount is non-zero',
+  },
+  '-1022': {
+    context: 'User Interface',
+    errorCodeDescription: 'Limit does not exist',
+  },
+  '-1030': {
+    context: 'Wallet Interface',
+    errorCodeDescription: 'Insufficient deduction amount',
+  },
+  '-1040': {
+    context: 'Verification Code',
+    errorCodeDescription: 'Verification Code Error',
+  },
+  '-1041': {
+    context: 'All interfaces',
+    errorCodeDescription: 'Illegal domain name',
+  },
+};
